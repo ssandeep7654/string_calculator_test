bb0cb0846fbe2b2a075674f05ed88bdcf8ca64bb
Sandeep Singh <sandeepsinghsandeep547@gmail.com>
2026-02-02 22:57:07 +0530

    Handle negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -10,6 +10,9 @@ class StringCalculator
     end
   
     nums = numbers.split(/#{delimiter}/).map(&:to_i)
+    negatives = nums.select { |n| n < 0 }
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
+
     nums.sum
-  end  
+  end 
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -25,5 +25,9 @@ RSpec.describe StringCalculator do
     it 'supports custom delimiter' do
       expect(StringCalculator.add("//;\n1;2")).to eq(3)
     end
+    
+    it 'throws an exception for negative numbers' do
+      expect { StringCalculator.add("1,-2,3,-4") }.to raise_error("negative numbers not allowed -2,-4")
+    end
   end
 end
